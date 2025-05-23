47270c7183944ca29fba4e9e8fa3a94d1462d887
FullstackTerm1nator <mikhail.shevchenko.1996@inbox.ru>
2025-05-23 19:30:10 +0300

    Замечания исправлены

--- 1-map/app.js
+++ 1-map/app.js
@@ -6,32 +6,6 @@ const objectList = [
     { id: 1, name: 'Вася' },
 ];
 
-// Вариант 1
-
-// const newObjectList = objectList.filter((value, index, list) => 
-//     index === list.findIndex((t) => (
-//         t.id === value.id
-//     ))
-// )
-
-// console.log(newObjectList);
-
-// Вариант 2
-
-// const array = [];
-
-// const uniqueObjects = objectList.map(obj => {
-//     if (!array.includes(obj.id)) {
-//     array.push(obj.id);
-//     return obj;
-//     }
-//     return null;
-// }).filter(Boolean);
-
-// console.log(uniqueObjects);
-
-// Вариант 3
-
 const setItem = new Set();
 
 const uniqueObjects = objectList.map(obj => {
@@ -43,7 +17,3 @@ const uniqueObjects = objectList.map(obj => {
 }).filter(Boolean);
 
 console.log(uniqueObjects);
-
-// Не совсем понятно зачем в этом задании нужен Set, ведь он по сути является просто вместилищем id объектов.
-// Таким же образом мы можем сделать вместо Set массив пустой и туда складывать id.
-// Есть ли какое-то преимущество использовать именно Set в этом примере?
--- 2-dice/app.js
+++ 2-dice/app.js
@@ -7,7 +7,11 @@ dice, который надо бросить: d4, d6, d8, d10, d12, d16, d20 и
 
 function castDice(dice) {
     const diceNumber = Number(dice.slice(1, Infinity));
-    return Math.floor(Math.random() * (diceNumber) + 1);
+    const diceArr = [4, 6, 8, 10, 12, 16, 20].filter((num) => num === diceNumber);
+    if (diceArr.length === 1) {
+        return Math.floor(Math.random() * (diceNumber) + 1);
+    }
+    return `Значения ${dice} не существует`;
 }
 
 console.log(castDice('d4'));
